fd8977249c96afa2cdc0671cf0223d9331a06756
Bhaswath Datla <bdatla03@uw.edu>
2025-12-20 11:45:00 -0800

    Implement ServerCheckResult telemetry data model

---
+++ src/NetworkMonitor.java
@@ -0,0 +1,19 @@
+﻿package engine;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+class ServerCheckResult {
+    String serverAddress;
+    long timestampMillis;
+    long latencyMillis;
+    boolean reachable;
+
+    ServerCheckResult(String serverAddress, long timestampMillis, long latencyMillis, boolean reachable) {
+        this.serverAddress = serverAddress;
+        this.timestampMillis = timestampMillis;
+        this.latencyMillis = latencyMillis;
+        this.reachable = reachable;
+    }
+}
